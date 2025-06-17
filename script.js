f199b0357a944dbbb61775b5ef3c2aff0257464c
mariomujica99 <mariomujica99@gmail.com>
2025-06-16 20:42:01 -0500

    Inital JavaScript code. Implementing API, and movie search feature.

--- script.js
+++ script.js
@@ -8,16 +8,46 @@ const search = document.getElementById("query");
 
 
 returnMovies(APILINK)
-function returnMovies(url) {
-  fetch(url).then(res => res.json())
-  .then(funtion(data)) {
-    console.log(data.results);
-    data.results.forEach(element => {
-      const div_card = document.createElement('div');
-      const div_row = document.createElement('div');
-      const div_column = document.createElement('div');
-      const div_image = document.createElement('img');
-      const div_title = document.createElement('h3');
+  function returnMovies(url) {
+    fetch(url).then(res => res.json()).then(function(data) {
+      console.log(data.results);
+      data.results.forEach(element => {
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class', 'row');
+
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class', 'column');
+
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
+
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
+
+        const title = document.createElement('p');
+        title.setAttribute('id', 'title');
+
+        title.innerHTML = `${element.title}`;
+        image.src = IMG_PATH + element.poster_path;
+
+        div_card.appendChild(image);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+        main.appendChild(div_row);
+      });
     });
-  });
-}
+  }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = '';
+
+  const searchItem = search.value;
+
+  if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
+    search.value = "";
+  }
+});
